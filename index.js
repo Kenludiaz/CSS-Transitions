d8c7610e29bf5d036b41d585b99d78c2fe50ec1b
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-09-03 12:45:32 -0400

    Js free version

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var elements = document.querySelector(".blue .nav-list");
+// var elements = document.querySelector(".blue .nav-list");
 
-var myFunction = function() {
-    this.style.opacity = ".6";
-};
-let myOtherFunction = function() {
-    this.style.opacity = "1";
-}
+// var myFunction = function() {
+//     this.style.opacity = ".6";
+// };
+// let myOtherFunction = function() {
+//     this.style.opacity = "1";
+// }
 
-elements.addEventListener("mouseover", myFunction, false);
-elements.addEventListener("mouseout", myOtherFunction, false);
+// elements.addEventListener("mouseover", myFunction, false);
+// elements.addEventListener("mouseout", myOtherFunction, false);
