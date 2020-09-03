a4579a686cd34c2f2f9defb2a4e8bf9c6dde9702
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-09-03 14:29:44 -0400

    Made select all except hovered selector.

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-<<<<<<< HEAD
+
 // var elements = document.querySelector(".blue .nav-list");
 
 // var myFunction = function() {
+    
 //     this.style.opacity = ".6";
 // };
 // let myOtherFunction = function() {
 //     this.style.opacity = "1";
 // }
 
+// let toggleAll = function() {
+
+// } 
+// let unToggle = function() {
+
+// }
+
 // elements.addEventListener("mouseover", myFunction, false);
 // elements.addEventListener("mouseout", myOtherFunction, false);
-=======
-function gatherAll(this) {
-    console.log(this);
-}
->>>>>>> parent of c4c8217... Playing with JS
+
+
+// let navItem = document.querySelectorAll(".blue .nav-item");
+// Array.from(navItem, item => item.addEventListener("click", hovered, false));
+
+
+
+// function hovered(()=> {this.classList.toggle("nav-item-hover");}) {
+//     alert(this.value);
+//     // this.classList.toggle
+// }
+
